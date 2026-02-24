48d17afa3d8964d018012de9d44ff2ca6e38b62b
Saahil Borole <anansi@Storybook.local>
2026-02-24 14:11:48 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <div>
+                ClarifAI
+            </div>
+            <div className={"grid grid-flow-col grid-rows-3 gap-4"}>
+                <div className={""}>
+                    Fact Checking
+                    <p>Identify and correct Misinformation</p>
+                </div>
+                <div className={""}>
+                    Ad Blocker
+                    <p>Block intrusive Advertisements</p>
+                </div>
+                <div className={""}>
+                    Content Summary
+                    <p>Generate AI Summaries</p>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default App
